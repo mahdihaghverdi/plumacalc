39184f21acff26e2354c492530fb600929e36c59
Mahdi Haghverdi <mahdihaghverdiliewpl@gmail.com>
2022-12-13 10:48:11 +0330

    calculator is basically working

--- calculator/views.py
+++ calculator/views.py
@@ -1,5 +1,31 @@
+import postfixcalc
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from .forms import InputForm
 
 
 def calculate(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello")
+    match request.method:
+        case "GET":
+            form = InputForm()
+            return render(request, "calculator/index.html", {"form": form})
+        case "POST":
+            form = InputForm(request.POST)
+            postfix, answer, errors = [None] * 3
+            if form.is_valid():
+                try:
+                    postfix = postfixcalc.infix_to_postfix(form.cleaned_data["input"])
+                    answer = postfixcalc.evaluate(postfix)
+                except SyntaxError as e:
+                    errors = e
+                return render(
+                    request,
+                    "calculator/index.html",
+                    {
+                        "form": form,
+                        "postfix": postfix,
+                        "answer": answer,
+                        "errors": errors,
+                    },
+                )
