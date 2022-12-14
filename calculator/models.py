edc33f9bd7ce839efc9e65cde13e8154e926c30e
Mahdi Haghverdi <mahdihaghverdiliewpl@gmail.com>
2022-12-15 00:11:50 +0330

    define History model and migrate

--- calculator/models.py
+++ calculator/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class History(models.Model):
     input = models.CharField(max_length=250)
+    answer = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.input} = {self.answer}"
