380e1c9fbc6a8a6d169ecd6fce9f37b35d907c9c
Mahdi Haghverdi <mahdihaghverdiliewpl@gmail.com>
2022-12-14 09:15:25 +0330

    Customize `input` widget

--- calculator/forms.py
+++ calculator/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from django.forms import TextInput
 
 
 class InputForm(forms.Form):
-    input = forms.CharField()
+    input = forms.CharField(
+        label="Expression",
+        label_suffix=": ",
+        empty_value="Arithmetic Expression",
+        widget=TextInput(attrs={"class": "form-control"}),
+    )
