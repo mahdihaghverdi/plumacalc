eaa4515d7fa28127062d86a2215c2942982f11c9
= <=>
2022-12-16 20:20:56 +0330

    redesign history section and put copying button for it

--- calculator/static/js/script.js
+++ calculator/static/js/script.js
@@ -12,3 +12,18 @@ function display(value) {
     let input = document.getElementById('userinput').value;
     document.getElementById('userinput').value = input + value;
 }
+
+function copyText(all) {
+    if (all) {
+        const input = document.getElementById('input').innerText
+        const answer = document.getElementById('answer').innerText
+        navigator.clipboard.writeText(input + '=' + answer)
+        const btnText = document.getElementById('copy-all')
+        btnText.innerText = "Copied!"
+        return
+    }
+    const input = document.getElementById('input').innerText
+    navigator.clipboard.writeText(input)
+    const btn = document.getElementById('copy-input')
+    btn.innerText = 'Copied!'
+}
