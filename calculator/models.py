d8785cba377935fa603a3e060cb2a76512a36eb2
Mahdi Haghverdi <Mahdihaghverdiliewpl@gmail.com>
2022-12-17 11:41:20 +0330

    add a nullable filed to History: `errors`

--- calculator/models.py
+++ calculator/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class History(models.Model):
     input = models.CharField(max_length=250)
     answer = models.CharField(max_length=250)
+    errors = models.CharField(max_length=250, null=True)
     created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
